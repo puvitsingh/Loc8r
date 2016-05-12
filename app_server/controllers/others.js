12deef0774b368774a52a22e1faadec255aab300
puvitsingh <puvit.singh@gmail.com>
2016-05-12 17:49:16 -0400

    added chapter 4 changes

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,4 +1,4 @@
 /* GET 'about' page. */
-module.exports.about = function(req, res) {
-  res.render('index', { title: 'About' });
+module.exports.about = function(req, res){
+  res.render('generic-text', { title: 'About' });
 };
